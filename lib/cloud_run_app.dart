34cdccd4bb8c5a0ef7659125aab06357e5faeab0
Luke Pighetti <lukepighetti@gmail.com>
2019-08-30 11:56:51 -0400

    first commit

---
+++ lib/cloud_run_app.dart
@@ -0,0 +1,11 @@
+/// cloud_run_app
+///
+/// A Aqueduct web server.
+library cloud_run_app;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
